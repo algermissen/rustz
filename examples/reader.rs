443a76ac68bfdc0c5e8fb937bc174015bd73423a
Algermissen Jan <jan.algermissen@metro.de>
2017-06-12 23:39:13 +0200

    Using flatmap in reader example

--- examples/reader.rs
+++ examples/reader.rs
@@ -4,6 +4,7 @@ use rustz::Reader;
 use std::vec::Vec;
 
 
+#[derive(Debug)]
 struct User {
     id: i32,
     some_val: i32,
@@ -13,8 +14,10 @@ struct Project {
     id: i32,
 }
 
+#[derive(Debug,Clone)]
 struct KV {
-    state: Box<i32>,
+    //state: Box<i32>,
+    state: i32,
 }
 
 impl KV {
@@ -24,12 +27,14 @@ impl KV {
 }
 
 
+#[derive(Debug,Clone)]
 struct GraphDB {
-    state: Box<i32>,
+    //state: Box<i32>,
+    state: i32,
 }
 impl GraphDB {
     fn find_all(&self, space: &str, key: i32) -> Vec<i32> {
-        vec![1, 2, 3]
+        vec![1, 2]
     }
 }
 
@@ -39,7 +44,10 @@ struct Env {
 }
 
 fn get_kv<'a>() -> Reader<'a, Env, KV> {
-    Reader::new(|env: &Env| env.kv)
+    Reader::new(|env: &Env| env.kv.clone())
+}
+fn get_gdb<'a>() -> Reader<'a, Env, GraphDB> {
+    Reader::new(|env: &Env| env.gdb.clone())
 }
 
 fn get_user<'a>(user_id: i32) -> Reader<'a, Env, User> {
@@ -52,15 +60,30 @@ fn get_user<'a>(user_id: i32) -> Reader<'a, Env, User> {
                  })
 }
 
+fn get_friends<'a>(user_id: i32) -> Reader<'a, Env, Vec<User>> {
+    get_gdb().map(move |gdb: GraphDB| {
+                      let _ = gdb.find_all("friends", user_id);
+                      let u1 = User { id: 1, some_val: 2 };
+                      let u2 = User { id: 2, some_val: 4 };
+                      vec![u1, u2]
+                  })
+}
+
 
 fn main() {
-    let kv = KV { state: Box::new(1) };
-    let gdb = GraphDB { state: Box::new(1) };
+    //let kv = KV { state: Box::new(1) };
+    //let gdb = GraphDB { state: Box::new(1) };
+    //
+    let kv = KV { state: 1 };
+    let gdb = GraphDB { state: 1 };
     let env = Env { kv: kv, gdb: gdb };
 
-    //let r = get_user(10).flat_map(|a| get_other(a + 2000));
+    let user_reader = get_user(10);
+    let friends_reader = get_user(10).flat_map(|user| get_friends(user.id));
 
-    //let u = r.run(&env);
+    let user = user_reader.run(&env);
+    let friends = friends_reader.run(&env);
 
-    //println!("Result: {:?} ", u);
+    println!("User: {:?} ", user);
+    println!("Friends: {:?} ", friends);
 }
